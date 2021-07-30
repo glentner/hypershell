7729f777acccfd9f17a5402e2434494d9b703a47
Geoffrey Lentner <glentner@purdue.edu>
2021-07-30 15:50:54 -0400

    Add version qualifier

--- hypershell/__meta__.py
+++ hypershell/__meta__.py
@@ -5,7 +5,7 @@
 
 
 __appname__     = 'hypershell'
-__version__     = '2.0.0'
+__version__     = '2.0.0-b0'
 __authors__     = 'Geoffrey Lentner'
 __contact__     = 'glentner@purdue.edu'
 __license__     = 'Apache Software License'
