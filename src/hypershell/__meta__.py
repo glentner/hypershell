51f60ca2897edb8e0e1abb690f55eb95bd644de0
Geoffrey Lentner <glentner@purdue.edu>
2022-04-10 11:27:39 -0400

    Version 2.0.0-b8

--- src/hypershell/__meta__.py
+++ src/hypershell/__meta__.py
@@ -5,7 +5,7 @@
 
 
 __pkgname__     = 'hypershell'
-__version__     = '2.0.0-b7'
+__version__     = '2.0.0-b8'
 __authors__     = 'Geoffrey Lentner'
 __contact__     = 'glentner@purdue.edu'
 __license__     = 'Apache Software License'
