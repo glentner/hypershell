8e7a8cae38e13ff4fb49346296fc533fa57991a3
Geoffrey Lentner <glentner@purdue.edu>
2023-03-29 14:12:59 -0400

    [release] Version 2.2.0

--- src/hypershell/__init__.py
+++ src/hypershell/__init__.py
@@ -31,7 +31,7 @@ from hypershell.database.model import Task
 __all__ = ['HyperShellApp', 'main', '__version__', '__license__']
 
 # project metadata
-__version__     = '2.1.0'
+__version__     = '2.2.0'
 __authors__     = 'Geoffrey Lentner'
 __contact__     = 'glentner@purdue.edu'
 __license__     = 'Apache Software License'
