cdf6358062d6664121ac18744269b240da9414c6
Geoffrey Lentner <glentner@purdue.edu>
2024-05-26 13:09:48 -0400

    Update repo URL
    Signed-off-by: Geoffrey Lentner <glentner@purdue.edu>

--- src/hypershell/__init__.py
+++ src/hypershell/__init__.py
@@ -28,7 +28,7 @@ __all__ = ['HyperShellApp', 'main', '__version__']
 
 # project metadata
 __version__     = get_version('hyper-shell')
-__website__     = 'https://github.com/glentner/hyper-shell'
+__website__     = 'https://github.com/glentner/hypershell'
 __description__ = 'Process shell commands over a distributed, asynchronous queue.'
 __citation__    = """\
 @inproceedings{lentner_2022,
