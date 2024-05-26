05bd0fb90636270af15a4e9415989e392eba0c4b
Geoffrey Lentner <glentner@purdue.edu>
2024-05-25 23:46:01 -0400

    Add macOS-specific file paths
    This is a breaking change for config files go but should
    happen transparently to the user in most cases.
    
    Signed-off-by: Geoffrey Lentner <glentner@purdue.edu>

--- src/hypershell/core/platform.py
+++ src/hypershell/core/platform.py
@@ -4,10 +4,17 @@
 """Platform specific file paths and initialization."""
 
 
+# NOTE:
+# A lot of the work done in this core module is provided by CmdKit at this point.
+# For continuity and for fear of breaking other parts of the project we have decided
+# to leave this module in place for the time being.
+
+
 # standard libs
 import os
 import sys
 import ctypes
+import platform
 
 # external libs
 from cmdkit.config import Namespace
@@ -30,7 +37,7 @@ else:
         sys.exit(exit_status.bad_config)
 
 
-if os.name == 'nt':
+if platform.system() == 'Windows':
     is_admin = ctypes.windll.shell32.IsUserAnAdmin() == 1
     site = Namespace(system=os.path.join(os.getenv('ProgramData'), 'HyperShell'),
                      user=os.path.join(os.getenv('AppData'), 'HyperShell'),
@@ -49,7 +56,26 @@ if os.name == 'nt':
             'log': os.path.join(site.local, 'Logs'),
             'config': os.path.join(site.local, 'Config.toml')}
     })
-else:
+
+elif platform.system() == 'Darwin':
+    is_admin = os.getuid() == 0
+    site = Namespace(system='/', user=home, local=local_site)
+    path = Namespace({
+        'system': {
+            'lib': os.path.join(site['system'], 'Library', 'HyperShell'),
+            'log': os.path.join(site['system'], 'Library', 'Logs', 'HyperShell'),
+            'config': os.path.join(site['system'], 'Library', 'Preferences', 'HyperShell', 'config.toml')},
+        'user': {
+            'lib': os.path.join(site['user'], 'Library', 'HyperShell'),
+            'log': os.path.join(site['user'], 'Library', 'Logs', 'HyperShell'),
+            'config': os.path.join(site['user'], 'Library', 'Preferences', 'HyperShell', 'config.toml')},
+        'local': {
+            'lib': os.path.join(site['local'], 'Library'),
+            'log': os.path.join(site['local'], 'Logs'),
+            'config': os.path.join(site['local'], 'config.toml')}
+    })
+
+elif os.name == 'posix':  # NOTE: likely Linux
     is_admin = os.getuid() == 0
     site = Namespace(system='/', user=os.path.join(home, '.hypershell'),
                      local=local_site)
@@ -67,6 +93,11 @@ else:
             'log': os.path.join(site.local, 'log'),
             'config': os.path.join(site.local, 'config.toml')}
     })
+
+else:
+    print(f'{bold(magenta("CRITICAL"))} [{__name__}] '
+          f'Platform unrecognized ({platform.system()})', file=sys.stderr)
+    sys.exit(exit_status.bad_config)
 
 
 if 'HYPERSHELL_SITE' in os.environ:
