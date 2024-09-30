53f7fae9536ccc49da392fbffacd22d0dcb42d77
Geoffrey Lentner <glentner@purdue.edu>
2024-09-30 10:30:56 -0400

    Add fuzz/perf code to FSM
    Code commented out as either "# FUZZ:" or "# PERF:" for testing
    and profiling purposes. To add these feature during development
    simply uncomment the code blocks of each or both.
    
    Signed-off-by: Geoffrey Lentner <glentner@purdue.edu>

--- src/hypershell/core/fsm.py
+++ src/hypershell/core/fsm.py
@@ -9,10 +9,12 @@ from __future__ import annotations
 from typing import Dict, Callable, Type
 
 # standard libs
-# import time  # NOTE: commented section below
-# import random
 from enum import Enum
 from abc import ABC
+# FUZZ: import time
+# FUZZ: import random
+# PERF: from collections import defaultdict
+# PERF: from time import perf_counter
 
 # internal libs
 from hypershell.core.exceptions import write_traceback
@@ -38,6 +40,10 @@ class StateMachine(ABC):
 
     __should_halt: bool = False
 
+    # NOTE: Only needed during performance profiling
+    # PERF: __perf_counter: float = 0
+    # PERF: __perf_data: Dict[int, float]
+
     def next(self) -> State:
         """Return next state (halt if necessary)."""
         previous_state = self.state
@@ -46,21 +52,28 @@ class StateMachine(ABC):
                 return self.states.HALT  # noqa: HALT defined in implemented State enums
             else:
                 action = self.actions.get(previous_state)
+                # PERF: self.__perf_counter = perf_counter()
                 next_state = action()
+                # PERF: self.__perf_data[previous_state.value] += perf_counter() - self.__perf_counter
         except Exception as error:
             log.critical(f'Uncaught exception from {self.__class__}')
             write_traceback(error, logger=log, module=__name__)
             raise
         else:
             # NOTE: Development aids not typically engaged
-            # time.sleep(random.uniform(0, 5))  # FUZZ
-            # log.devel(f'{self.__class__.__name__}: {previous_state} -> {next_state}')
+            # FUZZ: time.sleep(random.uniform(0, 5))  # FUZZ
+            # FUZZ: log.devel(f'{self.__class__.__name__}: {previous_state} -> {next_state}')
             return next_state
 
     def run(self) -> None:
         """Run machine until state is set to `HALT`."""
+        # PERF: self.__perf_data = defaultdict(lambda: 0)
         while self.state is not self.states.HALT:  # noqa: HALT defined in implemented State enums
             self.state = self.next()
+        # PERF: time_total = sum(self.__perf_data.values())
+        # PERF: for key, value in self.__perf_data.items():
+        # PERF:     t = 100 * value / time_total
+        # PERF:     log.trace(f'Profiler[{self.__class__.__name__}] {self.states(key).name}: {t:.3f}')
 
     def halt(self) -> None:
         """Set flag to signal for termination."""
